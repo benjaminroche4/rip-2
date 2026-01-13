31b6238aaf4f8d3c3abe5911c780bf155cd98e41
benjamin <benjamin.roche4@gmail.com>
2026-01-13 17:35:34 +0100

    WIP

--- src/Controller/Public/NewsletterController.php
+++ src/Controller/Public/NewsletterController.php
@@ -6,9 +6,11 @@ use App\Entity\Newsletter;
 use App\Form\NewsletterType;
 use Doctrine\ORM\EntityManagerInterface;
 use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -17,6 +19,7 @@ final class NewsletterController extends AbstractController
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
         private readonly TranslatorInterface $translator,
+        private readonly LoggerInterface $logger,
     )
     {
     }
@@ -51,18 +54,23 @@ final class NewsletterController extends AbstractController
             $this->entityManager->persist($newsletter);
             $this->entityManager->flush();
 
-            $resend->contacts->create([
-                'email' => $newsletter->getEmail(),
-                'segments' => [
-                    ['id' => '52a39bfb-e0fe-4aa6-8838-4555bc24f108'],
-                ],
-            ]);
+            try {
+                $resend->contacts->create([
+                    'email' => $newsletter->getEmail(),
+                    'segments' => [
+                        ['id' => '52a39bfb-e0fe-4aa6-8838-4555bc24f108'],
+                    ],
+                ]);
+            } catch (\Exception $e) {
+                $this->logger->error('Resend API error while adding contact: ' . $e->getMessage(), [
+                    'email' => $newsletter->getEmail(),
+                ]);
+            }
 
             $this->addFlash('newsletterSuccess', $this->translator->trans('newsletter.form.success.title'));
             return $this->redirectToRoute('app_newsletter');
         }
 
-        // Si le formulaire a été soumis mais invalide, retourner un status 422 pour Turbo
         $response = $this->render('public/newsletter/index.html.twig', [
             'newsletterForm' => $form->createView(),
         ]);
