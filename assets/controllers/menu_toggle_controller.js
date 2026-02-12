3d444c969d5f2b4d1f11d56bb691df00a105d318
benjamin <benjamin.roche4@gmail.com>
2026-02-12 11:22:12 +0100

    WIP

---
+++ assets/controllers/menu_toggle_controller.js
@@ -0,0 +1,41 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static targets = ['content', 'icon'];
+
+    connect() {
+        const isMobile = window.innerWidth < 768;
+
+        this.contentTarget.addEventListener('transitionend', () => {
+            if (this.open) {
+                this.contentTarget.style.maxHeight = 'none';
+            }
+        });
+
+        if (isMobile) {
+            this.open = false;
+            this.contentTarget.style.maxHeight = '0';
+            this.contentTarget.style.opacity = '0';
+        } else {
+            this.open = true;
+            this.iconTarget.classList.add('rotate-180');
+        }
+    }
+
+    toggle() {
+        this.open = !this.open;
+
+        if (this.open) {
+            this.contentTarget.style.maxHeight = this.contentTarget.scrollHeight + 'px';
+            this.contentTarget.style.opacity = '1';
+            this.iconTarget.classList.add('rotate-180');
+        } else {
+            this.contentTarget.style.maxHeight = this.contentTarget.scrollHeight + 'px';
+            requestAnimationFrame(() => {
+                this.contentTarget.style.maxHeight = '0';
+                this.contentTarget.style.opacity = '0';
+            });
+            this.iconTarget.classList.remove('rotate-180');
+        }
+    }
+}
