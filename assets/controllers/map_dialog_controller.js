3012f633b9cc9852a2e1ac070b5de66268d46b46
benjamin <benjamin.roche4@gmail.com>
2026-04-20 11:24:18 +0200

    WIP

--- assets/controllers/map_dialog_controller.js
+++ assets/controllers/map_dialog_controller.js
@@ -6,6 +6,7 @@ export default class extends Controller {
 
     open() {
         this.dialogTarget.showModal()
+        this.dialogTarget.focus()
         document.body.style.overflow = 'hidden'
         document.addEventListener('keydown', this.#handleKeydown)
         requestAnimationFrame(() => {
